e5638df89bc62da05dfb3be879e2e356cf3ad81b
Dan Wells <danwells396@gmail.com>
2019-02-28 18:53:17 +0000

    moving things into class methods. adding options for more flexible file handling

--- csv_listener.py
+++ csv_listener.py
@@ -23,50 +23,39 @@ elif os.name == 'posix':
 #script, csv_file = sys.argv
 
 class CsvListener(tk.Tk):
-    def __init__(self, csv_file, save_fn=None, win_title="CSV Listener", *args, **kwargs):
+    def __init__(self, csv_file, csv_out=None, disp_rows=10, fn_exclude='Exclude', fn_file_name='File Name', audio_path=None, win_title="CSV Listener", *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
 
         self.win_title = win_title
         self.csv_file = csv_file
-        self.save_fn = save_fn
+        self.csv_out = csv_out
+        self.disp_rows = disp_rows
+        self.fn_exclude = fn_exclude
+        self.fn_file_name = fn_file_name
+        self.audio_path = audio_path
 
         # set title on root window
         self.title("{0} - {1}".format(self.win_title, self.csv_file))
         # make floating window
         self.attributes('-type', 'utility')
 
-        with open(csv_file) as inf:
-            reader = csv.DictReader(inf)
-            csv_header = reader.fieldnames
-            csv_rows = [i for i in reader]
-        col_max_lens = {}
-        for col in csv_header:
-            col_max_lens[col] = max(max([len(i[col]) for i in csv_rows]), len(col))
-
         master_frame = tk.Frame(self)
         master_frame.pack(anchor=tk.NW)
+
+        # get various info from csv_file
+        self.read_csv()
 
         frame1 = tk.Canvas(master_frame)
         frame1.pack(anchor=tk.NW)
 
-        for i, col in enumerate(csv_header):
-            col_val = tk.Label(frame1, width=col_max_lens[col], padx=7, pady=7, relief=tk.RIDGE, text=col)
-            col_val.pack(anchor=tk.NW, side=tk.LEFT)
-        if 'Exclude' not in csv_header:
-            exclude_idx = len(csv_header) + 1
-            col_val = tk.Label(frame1, padx=7, pady=7, relief=tk.RIDGE, text='Exclude')
-            col_val.pack(anchor=tk.NW, side=tk.LEFT)
-            exclude_width = col_val.winfo_width()
-            play_idx = exclude_idx + 1
-        else:
-            play_idx = len(csv_header) + 1
-        col_val = tk.Label(frame1, padx=7, pady=7, relief=tk.RIDGE, text='Play audio')
-        col_val.pack(anchor=tk.NW, side=tk.LEFT)
-        play_width = col_val.winfo_width()
+        # draw header row into frame1
+        self.draw_header(frame1)
 
         frame2 = tk.Frame(master_frame)
         frame2.pack(anchor=tk.NW, fill=tk.X)
 
+        # want to be able to scroll through many rows but always see
+        # header: next bunch of tk objects is setting that up
         vsbar = tk.Scrollbar(frame2)
         vsbar.pack(side=tk.RIGHT, fill=tk.Y)
 
@@ -79,38 +68,17 @@ class CsvListener(tk.Tk):
         frame.rowconfigure(1, weight=1)
         frame.columnconfigure(1, weight=1)
 
-        self.exclude_vars = {}
-        for r, row in enumerate(csv_rows):
-            full_row = tk.Canvas(frame)
-            full_row.pack(anchor=tk.NW)
-            # add a warning about any duplicated rows: would need to have checkbutton
-            # selected for every instance to have any of them excluded in the end
-            exclude_var = tk.BooleanVar()
-            self.exclude_vars[row['File Name']] = exclude_var
-            exclude_button = tk.Checkbutton(full_row, padx=7, pady=7, relief=tk.RIDGE, variable=exclude_var, width=3)
-            for c, col in enumerate(row):
-                if col == 'Exclude':
-                    exclude_button.pack(anchor=tk.NW, side=tk.LEFT)
-                    if row['Exclude'] == 'x':
-                        #self.exclude_vars[row['File Name']].set(1)
-                        exclude_button.select()
-                else:
-                    col_val = tk.Label(full_row, width=col_max_lens[col], padx=7, pady=7, relief=tk.RIDGE, text=row[col])
-                    col_val.pack(anchor=tk.NW, side=tk.LEFT)
-            if 'Exclude' not in csv_header:
-                exclude_button.pack(anchor=tk.NW, side=tk.LEFT)
-            play_button = tk.Button(full_row, padx=7, pady=7, relief=tk.RIDGE, text='Play', 
-                                    command=lambda x=row['File Name']: self.play_wav(x))
-            play_button.pack(anchor=tk.NW, side=tk.LEFT)
+        # this puts data into the frame
+        self.draw_rows(frame)
 
+        # this creates the scrollable view over canvas in frame
         canvas.create_window(0, 0, anchor=tk.NW, window=frame)
         frame.update_idletasks()
         canvas.config(scrollregion=canvas.bbox("all"))
         
-        num_rows = len(csv_rows)
-        disp_rows = 10
-        if num_rows > disp_rows:
-            canvas_height = (frame.winfo_height() / num_rows) * disp_rows
+        num_rows = len(self.csv_rows)
+        if num_rows > self.disp_rows:
+            canvas_height = (frame.winfo_height() / num_rows) * self.disp_rows
         else:
             canvas_height = frame.winfo_height()
         canvas.configure(height=canvas_height)
@@ -119,50 +87,121 @@ class CsvListener(tk.Tk):
         frame3 = tk.Frame(master_frame)
         frame3.pack(anchor=tk.NW, fill=tk.X)
 
-        def load_file():
-            load_fn = filedialog.askopenfilename(initialdir=".", title="Select CSV file")
-            print(load_fn)
-        load_button = tk.Button(frame3, text="Load file", command=load_file, state=tk.DISABLED)
+        load_button = tk.Button(frame3, text="Load file", command=self.load_file, state=tk.DISABLED)
         load_button.pack(anchor=tk.NW)
         
-        def save_file(window=None):
-            with open(self.save_fn, 'w') as outf:
-                if 'Exclude' not in csv_header:
-                    csv_header.append('Exclude')
-                writer = csv.DictWriter(outf, fieldnames=csv_header, lineterminator='\n')
-                writer.writeheader()
-                for row in csv_rows:
-                    row['Exclude'] = self.exclude_vars[row['File Name']].get()
-                    writer.writerow(row)
-            if window is not None:
-                window.destroy()
-        def save_as(window=None):
-            self.save_fn = filedialog.asksaveasfilename(initialdir=".", title="Save to CSV file")
-            save_file(window)
-        def save_dialog():
-            if self.save_fn is None:
-                save_as()
-            else:
-                # popup to confirm saving over named save_fn
-                save_confirm = tk.Toplevel()
-                save_confirm.wm_title("Confirm save")
-                save_confirm.attributes('-type', 'dialog')
-                l = tk.Label(save_confirm, text="Save to {0}?".format(self.save_fn))
-                l.pack()
-                button_frame = tk.Frame(save_confirm)
-                button_frame.pack()
-                y_btn = tk.Button(button_frame, text="Save", command=lambda x=save_confirm:save_file(x))
-                y_btn.pack()
-                s_btn = tk.Button(button_frame, text="Save As", command=lambda x=save_confirm:save_as(x))
-                s_btn.pack()
-                n_btn = tk.Button(button_frame, text="Cancel", command=save_confirm.destroy)
-                n_btn.pack()
-        save_button = tk.Button(frame3, text="Save file", command=save_dialog)
+        save_button = tk.Button(frame3, text="Save file", command=self.save_dialog)
         save_button.pack(anchor=tk.NW)
 
         # make sure window shows all frames
         master_frame.update_idletasks()
         self.minsize(master_frame.winfo_width(), master_frame.winfo_height())
+
+    def read_csv(self):
+        with open(self.csv_file) as inf:
+            reader = csv.DictReader(inf)
+            self.csv_header = reader.fieldnames
+            self.csv_rows = []
+            #self.csv_rows = [i for i in reader]
+            for row in reader:
+                # concatenate audio paths
+                if self.audio_path is not None:
+                    wav_file = os.path.join(self.audio_path, row[self.fn_file_name])
+                    row[self.fn_file_name] = wav_file 
+                # convert existing exclude column to bools
+                if self.fn_exclude in self.csv_header:
+                    if row[self.fn_exclude] == 'True':
+                        row[self.fn_exclude] = True
+                    else:
+                        row[self.fn_exclude] = False
+                self.csv_rows.append(row)
+        self.col_max_lens = {}
+        for col in self.csv_header:
+            self.col_max_lens[col] = max(max([len(str(i[col])) for i in self.csv_rows]), len(col))
+
+    def draw_header(self, frame):
+        for i, col in enumerate(self.csv_header):
+            col_val = tk.Label(frame, width=self.col_max_lens[col], padx=7, pady=7, relief=tk.RIDGE, text=col)
+            col_val.pack(anchor=tk.NW, side=tk.LEFT)
+        if self.fn_exclude not in self.csv_header:
+            exclude_idx = len(self.csv_header) + 1
+            col_val = tk.Label(frame, padx=7, pady=7, relief=tk.RIDGE, text=self.fn_exclude)
+            col_val.pack(anchor=tk.NW, side=tk.LEFT)
+            self.exclude_width = col_val.winfo_width()
+            play_idx = exclude_idx + 1
+        else:
+            play_idx = len(self.csv_header) + 1
+        col_val = tk.Label(frame, padx=7, pady=7, relief=tk.RIDGE, text='Play audio')
+        col_val.pack(anchor=tk.NW, side=tk.LEFT)
+        self.play_width = col_val.winfo_width()
+
+    def draw_rows(self, frame):
+        # this keeps state of all exclude checkbuttons
+        self.exclude_vars = {}
+        for r, row in enumerate(self.csv_rows):
+            full_row = tk.Canvas(frame)
+            full_row.pack(anchor=tk.NW)
+            # TODO add warning about any duplicated rows: would need to have checkbutton
+            # selected for every instance to have any of them excluded in the end
+            exclude_var = tk.BooleanVar()
+            self.exclude_vars[row[self.fn_file_name]] = exclude_var
+            exclude_button = tk.Checkbutton(full_row, padx=7, pady=7, relief=tk.RIDGE, variable=exclude_var, width=3)
+            for c, col in enumerate(row):
+                if col == self.fn_exclude:
+                    exclude_button.pack(anchor=tk.NW, side=tk.LEFT)
+                    if row[self.fn_exclude]:
+                        exclude_button.select()
+                else:
+                    col_val = tk.Label(full_row, width=self.col_max_lens[col], padx=7, pady=7, relief=tk.RIDGE, text=row[col])
+                    col_val.pack(anchor=tk.NW, side=tk.LEFT)
+            if self.fn_exclude not in self.csv_header:
+                exclude_button.pack(anchor=tk.NW, side=tk.LEFT)
+            play_button = tk.Button(full_row, padx=7, pady=7, relief=tk.RIDGE, text='Play', 
+                                    command=lambda x=row[self.fn_file_name]: self.play_wav(x))
+            play_button.pack(anchor=tk.NW, side=tk.LEFT)
+
+    def load_file(self):
+        load_fn = filedialog.askopenfilename(initialdir=".", title="Select CSV file")
+        pass
+
+    def save_file(self, window=None):
+        with open(self.csv_out, 'w') as outf:
+            if self.fn_exclude not in self.csv_header:
+                self.csv_header.append(self.fn_exclude)
+            writer = csv.DictWriter(outf, fieldnames=self.csv_header, lineterminator='\n')
+            writer.writeheader()
+            for row in self.csv_rows:
+                row[self.fn_exclude] = self.exclude_vars[row[self.fn_file_name]].get()
+                writer.writerow(row)
+        if window is not None:
+            window.destroy()
+
+    def save_as(self, window=None):
+        self.csv_out = filedialog.asksaveasfilename(initialdir=".", title="Save to CSV file")
+        # hitting cancel returns an empty tuple, handle that
+        if self.csv_out:
+            self.save_file(window)
+        else:
+            self.csv_out = None
+
+    def save_dialog(self):
+        if self.csv_out is None:
+            self.save_as()
+        else:
+            # popup to confirm saving over named csv_out
+            save_confirm = tk.Toplevel()
+            save_confirm.wm_title("Confirm save")
+            save_confirm.attributes('-type', 'dialog')
+            l = tk.Label(save_confirm, text="Save to {0}?".format(self.csv_out))
+            l.pack()
+            button_frame = tk.Frame(save_confirm)
+            button_frame.pack()
+            y_btn = tk.Button(button_frame, text="Save", command=lambda x=save_confirm:self.save_file(x))
+            y_btn.pack(side=tk.LEFT)
+            s_btn = tk.Button(button_frame, text="Save As", command=lambda x=save_confirm:self.save_as(x))
+            s_btn.pack(side=tk.LEFT)
+            n_btn = tk.Button(button_frame, text="Cancel", command=save_confirm.destroy)
+            n_btn.pack(side=tk.LEFT)
 
     def play_wav(self, wav_fn, chunk_size=1024):
         '''
@@ -206,16 +245,14 @@ class CsvListener(tk.Tk):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="GUI tool for auditing lists of audio files.")
     parser.add_argument('csv_file', help="CSV file listing audio files to audit")
-    parser.add_argument('--csv_out', help="Output filename for modified CSV", required=False, default=None)
+    parser.add_argument('--csv_out', help="Output filename for modified CSV", default=None)
+    parser.add_argument('--disp_rows', help="Number of rows to show at once", default=10, type=int)
+    parser.add_argument('--fn_exclude', help="Field for exclude decisions. Will be added if not present", default='Exclude')
+    parser.add_argument('--fn_file_name', help="Field containing file name", default='File Name')
+    parser.add_argument('--audio_path', help="Path to audio files, use if full path not in CSV", default=None)
     args = parser.parse_args()
+    args_dict = vars(args)
 
-    csv_listener = CsvListener(args.csv_file, save_fn=args.csv_out)
-    #csv_listener = CsvListener(csv_file, save_fn=out_file)
-
-    #csv_listener.after(5000, lambda x=csv_listener.exclude_vars:print([i for i in x if x[i].get() == 1]))
-    #csv_listener.after(6000, lambda x=csv_listener.exclude_vars:print([i for i in x if x[i].get() == 0]))
+    csv_listener = CsvListener(**args_dict)
     csv_listener.mainloop()
     
-    #print([i for i in exclude_vars if exclude_vars[i]==1])
-    #print([i for i in csv_listener.exclude_vars if csv_listener.exclude_vars[i].get() == 1])
-

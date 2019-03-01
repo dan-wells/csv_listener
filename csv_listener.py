f5c62a8cadc2afe50f3fb79e1bd9beb6b5bda789
Dan Wells <danwells396@gmail.com>
2019-03-01 16:52:06 +0000

    added option to write comments against audio files and made exclude checkboxes optional

--- csv_listener.py
+++ csv_listener.py
@@ -26,18 +26,22 @@ elif os.name == 'posix':
         'Audio playback available on Windows through standard library module winsound.\n')
 
 class CsvListener(tk.Tk):
-    def __init__(self, csv_file, csv_out=None, disp_rows=10, fn_exclude='Exclude', 
-                 fn_file_name='File Name', audio_path=None, *args, **kwargs):
+    def __init__(self, csv_file, audio_path=None, do_exclude=False, do_comment=False, 
+                 fn_file_name='File Name', fn_exclude='Exclude', fn_comment='Comment',
+                 csv_out=None, disp_rows=10, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
 
         self.csv_file = csv_file
-        self.csv_out = csv_out
-        self.disp_rows = disp_rows
-        self.fn_exclude = fn_exclude
-        self.fn_file_name = fn_file_name
-        self.audio_path = audio_path
         # set title on root window
         self.title("CSV Listener - {0}".format(self.csv_file))
+        self.audio_path = audio_path
+        self.do_exclude = do_exclude
+        self.do_comment = do_comment
+        self.fn_file_name = fn_file_name
+        self.fn_exclude = fn_exclude
+        self.fn_comment = fn_comment
+        self.csv_out = csv_out
+        self.disp_rows = disp_rows
 
         master_frame = tk.Frame(self)
         master_frame.pack(anchor=tk.NW)
@@ -121,44 +125,55 @@ class CsvListener(tk.Tk):
         self.col_max_lens = {}
         for col in self.csv_header:
             self.col_max_lens[col] = max(max([len(str(i[col])) for i in self.csv_rows]), len(col))
+            # need wider for comment field: defaults 20 chars
+            self.col_max_lens[self.fn_comment] = 20
 
     def draw_header(self, frame):
         for i, col in enumerate(self.csv_header):
             col_val = tk.Label(frame, width=self.col_max_lens[col], padx=7, pady=7, relief=tk.RIDGE, text=col)
             col_val.pack(anchor=tk.NW, side=tk.LEFT)
-        if self.fn_exclude not in self.csv_header:
-            exclude_idx = len(self.csv_header) + 1
+        if (self.fn_exclude not in self.csv_header) and (self.do_exclude):
             col_val = tk.Label(frame, padx=7, pady=7, relief=tk.RIDGE, text=self.fn_exclude)
             col_val.pack(anchor=tk.NW, side=tk.LEFT)
-            self.exclude_width = col_val.winfo_width()
-            play_idx = exclude_idx + 1
-        else:
-            play_idx = len(self.csv_header) + 1
-        col_val = tk.Label(frame, padx=7, pady=7, relief=tk.RIDGE, text='Play audio')
+        if (self.fn_comment not in self.csv_header) and (self.do_comment):
+            col_val = tk.Label(frame, padx=7, pady=7, relief=tk.RIDGE, text=self.fn_comment, width=20)
+            col_val.pack(anchor=tk.NW, side=tk.LEFT)
+        col_val = tk.Label(frame, padx=7, pady=7, relief=tk.RIDGE, text='Click to play')
         col_val.pack(anchor=tk.NW, side=tk.LEFT)
-        self.play_width = col_val.winfo_width()
 
     def draw_rows(self, frame):
-        # this keeps state of all exclude checkbuttons
+        # these keep state of all exclude checkbuttons and comment text boxes
         self.exclude_vars = {}
+        self.comment_vars = {}
         for r, row in enumerate(self.csv_rows):
             full_row = tk.Canvas(frame)
             full_row.pack(anchor=tk.NW)
             # TODO add warning about any duplicated rows: would need to have checkbutton
             # selected for every instance to have any of them excluded in the end
-            exclude_var = tk.BooleanVar()
-            self.exclude_vars[row[self.fn_file_name]] = exclude_var
-            exclude_button = tk.Checkbutton(full_row, padx=7, pady=7, relief=tk.RIDGE, variable=exclude_var, width=3)
+            if self.do_exclude:
+                exclude_var = tk.BooleanVar()
+                self.exclude_vars[row[self.fn_file_name]] = exclude_var
+                exclude_button = tk.Checkbutton(full_row, padx=7, pady=7, relief=tk.RIDGE, variable=exclude_var, width=3)
+            if self.do_comment:
+                comment_var = tk.StringVar()
+                self.comment_vars[row[self.fn_file_name]] = comment_var
+                comment_box = tk.Entry(full_row, relief=tk.RIDGE, textvariable=comment_var)
             for col in self.csv_header:
-                if col == self.fn_exclude:
+                if (self.do_exclude) and (col == self.fn_exclude):
                     exclude_button.pack(anchor=tk.NW, side=tk.LEFT)
                     if row[self.fn_exclude]:
                         exclude_button.select()
+                elif (self.do_comment) and (col == self.fn_comment):
+                    comment_box.pack(anchor=tk.NW, side=tk.LEFT)
+                    if row[self.fn_comment]:
+                        comment_var.set(row[self.fn_comment])
                 else:
                     col_val = tk.Label(full_row, width=self.col_max_lens[col], padx=7, pady=7, relief=tk.RIDGE, text=row[col])
                     col_val.pack(anchor=tk.NW, side=tk.LEFT)
-            if self.fn_exclude not in self.csv_header:
+            if (self.do_exclude) and (self.fn_exclude not in self.csv_header):
                 exclude_button.pack(anchor=tk.NW, side=tk.LEFT)
+            if (self.do_comment) and (self.fn_comment not in self.csv_header):
+                comment_box.pack(anchor=tk.NW, side=tk.LEFT)
             play_button = tk.Button(full_row, padx=7, pady=7, relief=tk.RIDGE, text='Play', 
                                     command=lambda x=row['audio_path']: self.play_wav(x))
             play_button.pack(anchor=tk.NW, side=tk.LEFT)
@@ -186,12 +201,16 @@ class CsvListener(tk.Tk):
 
     def save_file(self):
         with open(self.csv_out, 'w') as outf:
-            if self.fn_exclude not in self.csv_header:
+            if (self.do_exclude) and (self.fn_exclude not in self.csv_header):
                 self.csv_header.append(self.fn_exclude)
+            if (self.do_comment) and (self.fn_comment not in self.csv_header):
+                self.csv_header.append(self.fn_comment)
             writer = csv.DictWriter(outf, fieldnames=self.csv_header, extrasaction='ignore', lineterminator='\n')
             writer.writeheader()
             for row in self.csv_rows:
+                # update any user-added values
                 row[self.fn_exclude] = self.exclude_vars[row[self.fn_file_name]].get()
+                row[self.fn_comment] = self.comment_vars[row[self.fn_file_name]].get()
                 writer.writerow(row)
 
     def play_wav(self, wav_fn):
@@ -221,9 +240,12 @@ class CsvListener(tk.Tk):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="GUI tool for auditing lists of audio files.")
     parser.add_argument('csv_file', help="CSV file listing audio files to audit")
-    parser.add_argument('--audio_path', help="Path to audio files, use if full path not in CSV", default=None)
+    parser.add_argument('-a', '--audio_path', help="Path to audio files, use if full path not in CSV", default=None)
+    parser.add_argument('-x', '--do_exclude', help="Configure tool for excluding audio files", action='store_true')
+    parser.add_argument('-c', '--do_comment', help="Configure tool for adding comments to audio files", action='store_true')
     parser.add_argument('--fn_file_name', help="Field containing audio file name. Default 'File Name'", default='File Name')
-    parser.add_argument('--fn_exclude', help="Field for exclude decisions. Will be added to output CSV if not present in input. Default 'Exclude'", default='Exclude')
+    parser.add_argument('--fn_exclude', help="Field for exclude decisions. Will be added to output CSV if not present in input and in exclude mode. Default 'Exclude'", default='Exclude')
+    parser.add_argument('--fn_comment', help="Field for text comments. Will be added to output CSV if not present in input and in comment mode. Default 'Comment'", default='Comment')
     parser.add_argument('--csv_out', help="Default output filename for modified CSV", default=None)
     parser.add_argument('--disp_rows', help="Number of rows visible at once in scrollable field. Default 10", default=10, type=int)
     args = parser.parse_args()
